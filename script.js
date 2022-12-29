9ce8f4ba39d586f73db7f39e129c56c9a4288ad9
iamonyourteam <jtneff92@gmail.com>
2022-12-28 22:22:21 -0500

    search and pebble products added

--- script.js
+++ script.js
@@ -4,24 +4,59 @@ fetch('data.json')
     function search() {
 		// Get the value of the search input
 		const searchValue = document.getElementById('search-input').value;
-	  
+		const tagValue = []
+		for(i=0;i<document.getElementsByClassName('tag').length;i++){
+			if(document.getElementsByClassName('tag')[i].checked == true){
+				tagValue.push(document.getElementsByClassName('tag')[i].value)
+			}
+		}
+		
 		// Filter the data array based on the search value, or display all of the data if the search input is empty
 		let filteredData;
-		if (searchValue === '') {
+		if (searchValue === '' && tagValue.length == 0) {
 		  filteredData = data;
-		} else {
+		} 
+		if(tagValue.length == 0) {
 		  filteredData = data.filter(item => {
 			return item.name.toLowerCase().includes(searchValue.toLowerCase());
 		  });
 		}
-	  
+		if(tagValue.length > 0){
+
+			if(searchValue === ''){
+			filteredData = data.filter(item => {
+				console.log(item.tags.toString().toLowerCase());
+				console.log(tagValue.toString().toLowerCase())
+				return item.tags.toString().toLowerCase().includes(tagValue.toString().toLowerCase());
+		
+			  });
+			} 
+			else {
+				filteredData = data.filter(item => {
+					
+					return item.name.toLowerCase().includes(searchValue.toLowerCase());
+				  });
+				  console.table(filteredData)
+				 
+				 
+				  filteredData = filteredData.filter(item => {
+					
+					return item.tags.toString().toLowerCase().includes(tagValue.toString().toLowerCase())
+				  });
+
+				
+			}
+		}
+		
+	//   console.table(filteredData)
 		// Clear the results container
 		document.getElementById('results').innerHTML = '';
 	  
 		// Add the filtered data to the results container
 		filteredData.forEach(item => {
 		  const resultElement = document.createElement('div');
-		  resultElement.innerHTML = `<img src="${item.photoUrl}" alt="${item.name}"><p>${item.name}</p>`;
+		  resultElement.innerHTML = 
+		  `<img src="${item.photoUrl}" alt="${item.name}"><p>${item.name}</p>`;
 		  document.getElementById('results').appendChild(resultElement);
 		});
 	  }
@@ -29,5 +64,9 @@ fetch('data.json')
 
     // Attach an event listener to the search input that calls the search function when the input value changes
     document.getElementById('search-input').addEventListener('input', search);
+	for(i=0;i<document.getElementsByClassName('tag').length;i++){
+		document.getElementsByClassName('tag')[i].addEventListener('change', search);
+	}
+	
 	search();  
 });
